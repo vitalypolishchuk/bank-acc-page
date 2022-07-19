c2c61ac1dcb3c14bbaee9d9486c847743a33a521
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-07-19 16:51:59 +0300

    'update'

--- script.js
+++ script.js
@@ -3,15 +3,52 @@
 DOC VARIABLES
 //////////////////////////
 */
+/// navigation ///
 const navBtn = document.getElementById("navBtn");
 const navLinks = document.getElementById("nav-links");
-/// navigation ///
-
+/// operations ///
+const operationsBtnContainer = document.querySelector(".operations-btns-container");
+const transferBtn = document.querySelector(".transfer");
+const loanBtn = document.querySelector(".loan");
+const closeBtn = document.querySelector(".close");
+const operationsInsideContainer = document.querySelectorAll(".operations-inside-container");
+const transferMoneyContainer = document.querySelector(".transfer-money-container");
+const loanContainer = document.querySelector(".loan-container");
+const closeContainer = document.querySelector(".close-container");
 /*
 //////////////////////////
-NAVIGATION
+EVENT LISTENERS
 //////////////////////////
 */
+/// navigation ///
 navBtn.addEventListener("click", function () {
   navLinks.classList.toggle("nav-links--visible");
+});
+/// operations ///
+transferBtn.addEventListener("click", function () {
+  operationsInsideContainer.forEach((selector) => {
+    selector.style.display = "none";
+  });
+  transferBtn.style.alignSelf = "flex-start";
+  loanBtn.style.alignSelf = "center";
+  closeBtn.style.alignSelf = "center";
+  transferMoneyContainer.style.display = "inline-block";
+});
+loanBtn.addEventListener("click", function () {
+  operationsInsideContainer.forEach((selector) => {
+    selector.style.display = "none";
+  });
+  transferBtn.style.alignSelf = "center";
+  loanBtn.style.alignSelf = "flex-start";
+  closeBtn.style.alignSelf = "center";
+  loanContainer.style.display = "inline-block";
+});
+closeBtn.addEventListener("click", function () {
+  operationsInsideContainer.forEach((selector) => {
+    selector.style.display = "none";
+  });
+  transferBtn.style.alignSelf = "center";
+  loanBtn.style.alignSelf = "center";
+  closeBtn.style.alignSelf = "flex-start";
+  closeContainer.style.display = "inline-block";
 });
